36e935a77c0bdbf17bbce8a0508e00640737e5ad
Matthew Jenner <matthewjenner@hotmail.com>
2023-03-14 09:52:25 +0000

    basic css added

--- src/Components/ArticleCard.jsx
+++ src/Components/ArticleCard.jsx
@@ -1,10 +1,12 @@
 function ArticleCard({article}) {
 
     return (
-        <li className="article_card">
+        <div className="article_card">
+            <img className = "article_image" src={article.article_img_url} alt={`article of ${article.title}`}></img>
+            <div className = "article_container">
             <h2>{article.title}</h2>
-            <img src={article.article_img_url} alt={`article of ${article.title}`}></img>
-        </li>
+            </div>
+        </div>
     )
 }
 
