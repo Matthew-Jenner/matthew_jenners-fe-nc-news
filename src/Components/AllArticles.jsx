d751afc7856a71eb2376bac807af4787b32a6b01
Matthew Jenner <matthewjenner@hotmail.com>
2023-03-14 10:15:20 +0000

    updated with loading screen

--- src/Components/AllArticles.jsx
+++ src/Components/AllArticles.jsx
@@ -4,14 +4,20 @@ import ArticleCard from "./ArticleCard"
 
 function AllArticles(){
 const [allArticles, setAllArticles] = useState([])
+const[isLoading, setIsLoading] = useState([true])
 
 useEffect(() => {
+    setIsLoading(true)
     getAllArticles().then((articlesFromAPi) => {
 setAllArticles(articlesFromAPi)
+setIsLoading(false)
     })
 }, [])
 
-return(
+return isLoading ? (
+    <h1> Loading....</h1>
+) : (
+
     <div>
     <section>
     <h2> Here are all of the articles:</h2>
