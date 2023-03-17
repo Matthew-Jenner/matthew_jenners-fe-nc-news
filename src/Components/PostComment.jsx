b02a497a9ccda6f122fa7a903e4f44d05e4d766e
Matthew Jenner <matthewjenner@hotmail.com>
2023-03-17 12:48:17 +0000

    added error message as advised

--- src/Components/PostComment.jsx
+++ src/Components/PostComment.jsx
@@ -5,6 +5,7 @@ import { UserContext } from "../contexts/Users";
 function PostComment({setAllComments, article_id}){
 const [newComment, setNewComment] = useState('')
 const [isSubmitDisabled, setIsSubmitDisabled] = useState(false);
+const [errorPost, setErrorPost] = useState(false);
 
 const { user } = useContext(UserContext);
 
@@ -21,8 +22,8 @@ setIsSubmitDisabled(true)
 
   })
 .catch((error) => {
-  const errorMessage = error.response.data.message || 'Something went wrong!';
-  alert(errorMessage);
+  setErrorPost(true)
+  setIsSubmitDisabled(false)
 });
 }
 
@@ -34,8 +35,9 @@ setIsSubmitDisabled(true)
           value={newComment}
           placeholder={"logged in users can write a comment..."}
           onChange={(event) => setNewComment(event.target.value)}
-        />
+          required/>
         <button type="submit" disabled={isSubmitDisabled}>Add comment</button>
+        {errorPost && <p className="error-message">Unable to post comment, there is an error</p>}
       </form>
         )
 }
